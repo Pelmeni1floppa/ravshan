8bb2f2cbf5abc0c584bc1bbc882667025d79e1c1
KXMXIS <94845425+Pelmeni1floppa@users.noreply.github.com>
2026-06-09 09:36:11 +0500

    Add files via upload

---
+++ WpfApp1/WpfApp1/Exit.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Exit.xaml
+    /// </summary>
+    public partial class Exit : Window
+    {
+        public Exit()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ну и пошёл нахуй");
+            Application.Current.Shutdown(); // Полное завершение приложения
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Ну и пошёл нахуй");
+            Application.Current.Shutdown(); // Полное завершение приложения
+        }
+    }
+}
